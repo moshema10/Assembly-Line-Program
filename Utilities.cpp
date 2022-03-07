9ea6587a031ff317e3eee514d51cd801f649909b
Shehtab Masud <77507757+moshema10@users.noreply.github.com>
2022-03-06 22:52:20 -0500

    Update Utilities.cpp

--- Utilities.cpp
+++ Utilities.cpp
@@ -1,10 +1,3 @@
-// Name: Shehtab Masud
-// Seneca Student ID: 119038206
-// Seneca email: smasud3@myseneca.ca
-// Date of completion: 11/20/2021
-//
-// I confirm that I am the only author of this file
-//   and the content was created entirely by me.
 
 
 #include <iostream>
